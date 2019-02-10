604cf448d5cc658e469edea641382cf5580ec874
sachin chinchwade <schinchwade@aurusinc.com>
2019-02-10 14:28:21 +0530

    initial commit

---
+++ src/main/java/bucket/commonbucket/CommonbucketApplication.java
@@ -0,0 +1,14 @@
+package bucket.commonbucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonbucketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonbucketApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/bucket/commonbucket/CommonbucketApplicationTests.java
@@ -0,0 +1,17 @@
+package bucket.commonbucket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommonbucketApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
